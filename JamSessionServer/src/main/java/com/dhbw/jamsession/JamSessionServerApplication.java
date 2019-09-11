e199ca2814c06184e54936ebcf3623b7a5cfeef7
lheubach <lukas.heubach@gmx.de>
2019-09-11 12:47:43 +0200

    Setup new spring boot project

---
+++ JamSessionServer/src/main/java/com/dhbw/jamsession/JamSessionServerApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.jamsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamSessionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamSessionServerApplication.class, args);
+	}
+
+}
